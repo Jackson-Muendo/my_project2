16658295ba4dd1ae4f2c939a8345ac664a1b5d64
Jackson-Muendo <91467851+Jackson-Muendo@users.noreply.github.com>
2022-11-27 09:52:56 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '@styles/globals.css'
+<!--import '@style.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default Application
+export default Application-->
